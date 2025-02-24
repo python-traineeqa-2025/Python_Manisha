86022cd00fee70d0c7f4d0b835311d40f7d4d24a
ghartimaneesa@gmail.com <manishag@infinite.com>
2025-02-24 10:12:19 +0545

    First Commit

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
+#keyword
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import keyword
 
+print(keyword.kwlist)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+snake_case = [] #varaible ko name set garda use garcha
+camel_case = []  #Class set garda use garcha.
 
+#Datatypes
+#INT, float, string, boolean, list, tuple, set and dictionary
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+#input
+a = int(input("Enter the value of a"))
+b = int(input("Enter the value of b"))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+cal = int(input("option1:add option2: Subtract option3:Multiplication"))
+
+if cal == 1:
+    print(a+b)
+elif cal == 2:
+    print(a-b)
+else:
+    print(a*b)
