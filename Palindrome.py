3dfb36a8d40db619dda3a88a8ea0002d1556bc72
ghartimaneesa@gmail.com <manishag@infinite.com>
2025-02-24 11:01:28 +0545

    Palindrome checked

---
+++ Palindrome.py
@@ -0,0 +1,33 @@
+#
+# word = str(input("Enter a word that you want to check the palindrome"))
+# reverse = word[::-1]
+# print(reverse)
+#
+# if  str(reverse) == str(word):
+#     print("Palindrome")
+# else:
+#     print("Not palindrome")
+
+#number
+print('Enter the number that you want to check palindrome')
+
+number = int(input())
+temp = number
+rev = 0
+
+while number> 0:
+    digit = number % 10
+    rev = rev * 10 + digit
+
+    #it will remove the last digit from the number
+    number = number // 10
+
+if temp == rev:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
+
+
+
+
+
